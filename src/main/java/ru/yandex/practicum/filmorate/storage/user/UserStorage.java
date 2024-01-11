66c20292493ef9f05406b269ddc6920827054504
RatonLaveur <AjaxRid@yandex.ru>
2024-01-11 12:42:28 +0300

    Sprint_10#7-all-user-and-films-tests-passed

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -22,6 +22,7 @@ public interface UserStorage {
     User deleteFriend(User user, long friendId);
 
     List<Long> getCommonFriends(User user1, User user2);
-    
+
     User getUserById(long id);
+    
 }
