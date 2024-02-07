32b3f7299367df3553ffd6475b60bf5b4d8e5546
RatonLaveur <AjaxRid@yandex.ru>
2024-02-07 15:49:46 +0300

    data-base-added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO "users" (user_id, name, email, login, birthday) VALUES (1,'Alexandr', 'alex@yandex.ru', 'alex07', '1990-12-30');
+INSERT INTO "users" (user_id, name, email, login, birthday) VALUES (2,'Boris', 'bobr@mail.ru', 'bob15', '1970-05-09');
+INSERT INTO "users" (user_id, name, email, login, birthday) VALUES (3,'Anna', 'anyuta@gmail.com', 'anet77', '1995-08-13');
+
+INSERT INTO "films" VALUES (1, 'От заката до рассвета', 'Тут какое-то описание', '1990-10-01', 95);
